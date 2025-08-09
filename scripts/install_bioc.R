b6aa9ea94a6484e29023788957e6725b7ea7a289
Mateus Stocco <90330634+Mateusinhoo@users.noreply.github.com>
2025-08-09 16:24:09 -0600

    Update install_bioc.R

--- scripts/install_bioc.R
+++ scripts/install_bioc.R
@@ -1,15 +1,20 @@
-pkgs <- c(
-  "limma",
-  "minfi",
-  "IlluminaHumanMethylation450kanno.ilmn12.hg19",
-  "IlluminaHumanMethylation450kmanifest",
-  "RColorBrewer",
-  "missMethyl",
-  "matrixStats",
-  "minfiData",
-  "Gviz",
-  "DMRcate",
-  "stringr"
-)
+light_pkgs <- c(
+  "RColorBrewer",  # plotting
+  "stringr"        # already in env
 
-BiocManager::install(pkgs, ask = FALSE, update = FALSE)
+if (!"BiocManager" %in% rownames(installed.packages())) {
+  install.packages("BiocManager", repos = "https://cloud.r-project.org")
+}
+
+# Prefer macOS binaries from CRAN when possible
+if (Sys.info()[["sysname"]] == "Darwin") {
+  options(pkgType = "mac.binary")
+}
+
+need <- setdiff(light_pkgs, rownames(installed.packages()))
+if (length(need)) {
+  BiocManager::install(need, ask = FALSE, update = FALSE,
+                       Ncpus = max(1, parallel::detectCores() - 1))
+} else {
+  message("All requested CRAN/Bioc light packages already installed.")
+}
